0149845b3d20a2056ebf0159b6673ad7be2ea392
Ersin Sular <ersinsular1994@gmail.com>
2021-01-19 21:45:45 +0300

    3 Ders Toplamı

---
+++ KampIntro/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            string kategoriEtiketi = "Kategori";  // metinsel veri
+            int ogrenciSayisi = 32000;  // Sayısal Veri
+            double faizOrani = 1.45;   //ondalıklı sayı
+            bool sistemeGirisYapmismi = true;  //iki durum giriş yapmışmı yapmamışmı
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Buotnu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+
+
+            //  if () //şart 
+
+            // { // şart geçerli ise buraya yazılır } 
+
+            if (sistemeGirisYapmismi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+
+            }
+            Console.WriteLine(kategoriEtiketi);
+
+
+
+
+
+        }
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya Elması";
+
+            string[] meyveler = new string[] { };
+
+            Urün urun1 = new Urün();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elmesı";
+
+            Urün urun2 = new Urün();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urün[] urunler = new Urün[] { urun1,urun2 };
+
+            foreach (Urün urun in urunler)
+
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("---------------------");
+            }
+
+            Console.WriteLine("------------Metotlar-------------");
+
+
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil Armut", 12, 10);
+            sepetManager.Ekle2("Elma", "Yeşil Elma", 12, 9);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır Karpuzu", 12, 8);
+        }
+    }
+}
